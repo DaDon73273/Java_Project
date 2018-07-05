1b47646012554b93b40ebca0480440d60d46df16
mashaupfarelo34@gmail.com <mashaupfarelo34@gmail.com>
2018-07-05 17:43:30 +0200

    Co-Authored-By: dadon73273 <dadon73273@users.noreply.github.com>

--- Travelstart/src/main/resources/static/TravelstartClient/src/app/components/user/user.component.ts
+++ Travelstart/src/main/resources/static/TravelstartClient/src/app/components/user/user.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/Router';
 
+
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
- users:User[];
+ 
 
   constructor() { }
 
