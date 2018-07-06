4ac747ebdd9a75d2050fa8f1bcf0dbe0f2650bc9
Your Name <you@example.com>
2018-07-06 15:00:16 +0200

    DownGrading to angular 5
    Co-Authored-By: dadon73273 <dadon73273@users.noreply.github.com>

--- Travelstart/src/main/resources/static/TravelClient/src/app/app.module.ts
+++ Travelstart/src/main/resources/static/TravelClient/src/app/app.module.ts
@@ -1,31 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import {HttpModule} from '@angular/http';
+
 
 import { AppComponent } from './app.component';
-import { ListusersComponent } from './components/listusers/listusers.component';
-import { UserFormComponent } from './components/user-form/user-form.component';
-import { UserService } from './shared-service/user.service';
 
 
-const appRoute:Routes=[
- {path:'',component:ListusersComponent},
- {path:'op',component:UserFormComponent}
-]
 @NgModule({
   declarations: [
-    AppComponent,
-    ListusersComponent,
-    UserFormComponent,
-   
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    HttpModule,
-    RouterModule.forRoot(appRoute)
+    BrowserModule
   ],
-  providers: [UserService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
