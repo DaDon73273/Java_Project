be9c246c4515ec147cf17784dd6799bb1048fa81
Your Name <you@example.com>
2018-07-06 15:43:53 +0200

    angular 4
    Co-Authored-By: dadon73273 <dadon73273@users.noreply.github.com>

--- Travelstart/src/main/resources/static/TravelClient/src/app/app.module.ts
+++ Travelstart/src/main/resources/static/TravelClient/src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule,Routes} from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 import { ViewUsersComponent } from './components/view-users/view-users.component';
 import { UserFormComponent } from './components/user-form/user-form.component';
+import { UserService } from './shared-service/user.service';
+import {HttpModule} from '@angular/http';
 
-
+const appRoute:Routes=[
+  {path:'',component:ViewUsersComponent},
+  {path:'register',component:UserFormComponent}
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,9 +20,11 @@ import { UserFormComponent } from './components/user-form/user-form.component';
     UserFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    RouterModule.forRoot(appRoute)
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- Travelstart/src/main/resources/static/TravelClient/src/app/components/view-users/view-users.component.ts
+++ Travelstart/src/main/resources/static/TravelClient/src/app/components/view-users/view-users.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../shared-service/user.service';
+import { User } from '../../model-classes/user';
+import { error } from '../../../../node_modules/protractor';
 
 @Component({
   selector: 'app-view-users',
@@ -6,10 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-users.component.css']
 })
 export class ViewUsersComponent implements OnInit {
-
-  constructor() { }
+  private user:User[];
+  constructor(private _userService:UserService) { }
 
   ngOnInit() {
+    this._userService.getUsers()
+    .subscribe(data=>{
+      console.log(data);
+    },(error)=>{
+      console.log(error);
+    })
   }
 
 }
