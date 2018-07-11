2e5676922a8e769e39b5f2a93b1e8fd034aa45c5
Pfarelo <mashaupfarelo34@gmail.com>
2018-07-11 15:16:44 +0200

    aaass

--- Travelstart/src/main/resources/static/TravelClient/src/app/components/sign-up/sign-up.component.ts
+++ Travelstart/src/main/resources/static/TravelClient/src/app/components/sign-up/sign-up.component.ts
@@ -1,21 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { User } from '../../model-classes/user';
 import {NgForm} from '@angular/forms';
 
 import {Router} from '@angular/router';
 import { UserService } from '../../shared-service/user.service';
 import { Title } from '../../../../node_modules/@angular/platform-browser';
+
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
-  private user:User;
+   
   constructor(private _userService:UserService,private _router:Router) { }
 
   ngOnInit() {
-    this.user=this._userService.getter()
+   // this.objUser=this._userService.getter();
     
   }
 
